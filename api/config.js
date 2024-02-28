31ac38fa6a2d0d898bcd5ddcae23b77a2b683555
karivellajames <karivellajames2022@gmail.com>
2024-02-28 15:33:40 +0530

    book store

--- api/config.js
+++ api/config.js
@@ -3,5 +3,3 @@ export const PORT = 4000;
 export const mongoDBURL =
   "mongodb+srv://karivellajames2022:Q1usBFoj2eYrj51n@book-store-mern.mess4vb.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN";
 
-//mongodb+srv://karivellajames2022:Q1usBFoj2eYrj51n@book-store-mern.mess4vb.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN
-//Q1usBFoj2eYrj51n
